0979a0e76d7c1ae77dff13caa82587cc87f4df32
Igrek <igrek.s@o2.pl>
2015-05-25 14:17:18 +0200

    dupa

--- version.h
+++ version.h
@@ -1,8 +1,8 @@
-//v 1 20 5
-// -- 02:20:04, 25.05.2015 --
+//v 1 20 7
+// -- 14:16:05, 25.05.2015 --
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION "1.20.5"
+#define VERSION "1.20.7"
 
 #endif
